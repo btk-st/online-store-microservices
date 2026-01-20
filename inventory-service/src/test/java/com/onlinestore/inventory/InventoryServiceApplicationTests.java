1688154cf51632eedd58f6a4feaa103dcbb3a894
a.volkov <btk.st@yandex.ru>
2026-01-20 14:19:55 +0300

    inventory-service init

---
+++ inventory-service/src/test/java/com/onlinestore/inventory/InventoryServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.onlinestore.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
