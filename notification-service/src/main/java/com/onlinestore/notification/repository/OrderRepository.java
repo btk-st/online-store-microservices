d7e0501bcda58ae9daf74c81d70bf2c2cac7a93d
a.volkov <btk.st@yandex.ru>
2026-01-28 14:33:16 +0300

    notification service: redis cache

--- notification-service/src/main/java/com/onlinestore/notification/repository/OrderRepository.java
+++ notification-service/src/main/java/com/onlinestore/notification/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.onlinestore.notification.repository;
 
 import com.onlinestore.notification.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,10 @@ import java.util.UUID;
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
     List<OrderEntity> findByOrderId(UUID orderId);
     List<OrderEntity> findByUserId(UUID userId);
+    @Query("SELECT DISTINCT o.orderId FROM OrderEntity o")
+    List<UUID> findAllOrderIds();
+    @Query("SELECT DISTINCT o.orderId FROM OrderEntity o WHERE o.userId = :userId")
+    List<UUID> findOrderIdsByUserId(UUID userId);
 
     boolean existsByOrderIdAndProductId(UUID orderId, UUID productId);
 }
--- notification-service/src/main/java/com/onlinestore/notification/service/OrderService.java
+++ notification-service/src/main/java/com/onlinestore/notification/service/OrderService.java
@@ -6,48 +6,57 @@ import com.onlinestore.notification.entity.OrderEntity;
 import com.onlinestore.notification.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.parsers.ReturnTypeParser;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     public List<OrderDto> getAllOrders() {
-        // Группируем все записи по order_id
-        Map<UUID, List<OrderEntity>> groupedOrders = orderRepository.findAll()
-                .stream()
-                .collect(Collectors.groupingBy(OrderEntity::getOrderId));
-
-        // Преобразуем в список DTO
-        return groupedOrders.entrySet().stream()
-                .map(entry -> mapToOrderDto(entry.getKey(), entry.getValue()))
-                .collect(Collectors.toList());
+        List<OrderDto> orders = new ArrayList<>();
+        var allOrderIds = orderRepository.findAllOrderIds();
+        for (UUID orderId: allOrderIds) {
+            var order = getItemsByOrderId(orderId);
+            if (order != null) {
+                orders.add(order);
+            }
+        }
+        return orders;
     }
 
     public OrderDto getItemsByOrderId(UUID orderId) {
+        String key = "order:" + orderId;
+        OrderDto cached = (OrderDto) redisTemplate.opsForValue().get(key);
+        if (cached != null) {
+            return cached;
+        }
+        //Если не нашли в кеше
         List<OrderEntity> orders = orderRepository.findByOrderId(orderId);
         if (orders.isEmpty()) {
             return null;
         }
-        return mapToOrderDto(orderId, orders);
+        var orderDto = mapToOrderDto(orderId, orders);
+        //Сохраняем в кеш
+        redisTemplate.opsForValue().set(key, orderDto);
+        return orderDto;
     }
 
     public List<OrderDto> getOrdersByUserId(UUID userId) {
-        // Находим все записи пользователя и группируем по order_id
-        Map<UUID, List<OrderEntity>> userOrdersGrouped = orderRepository.findByUserId(userId)
-                .stream()
-                .collect(Collectors.groupingBy(OrderEntity::getOrderId));
-
-        return userOrdersGrouped.entrySet().stream()
-                .map(entry -> mapToOrderDto(entry.getKey(), entry.getValue()))
-                .collect(Collectors.toList());
+        List<OrderDto> orders = new ArrayList<>();
+        var orderIdsByUserId = orderRepository.findOrderIdsByUserId(userId);
+        for (UUID orderId: orderIdsByUserId) {
+            var order = getItemsByOrderId(orderId);
+            if (order != null) {
+                orders.add(order);
+            }
+        }
+        return orders;
     }
 
     private OrderDto mapToOrderDto(UUID orderId, List<OrderEntity> orderEntities) {
